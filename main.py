6e0d8f3ee246c1e33ec3c52d616f8a4c93b091ea
hayusom <hayusomartin@gmail.com>
2024-12-10 11:17:44 +0100

    añadimos datos ficticios de usuarios y transacciones, y he creado 2 endpoints mas: usuarios y transacciones

--- main.py
+++ main.py
@@ -16,7 +16,30 @@ from fastapi import FastAPI
 aplicacion = FastAPI()
 
 
-#Creando un endpoint para hacer pruebas..:
+#Creando datos ficticios
+
+usuarios = [
+    
+{'id_usuario' : 1 , 'nombre' : 'Ayuso' , 'edad' : 33 , 'ubicacion' : 'Barcelona'},
+{'id_usuario' : 2 , 'nombre' : 'Alberto' , 'edad' : 60 , 'ubicacion' : 'Valladolid'},
+{'id_usuario' : 3 , 'nombre' : 'Reyes' , 'edad' : 62 , 'ubicacion' : 'Soria'},
+
+]
+
+transacciones = [
+    
+{'id_transiccion' : 1 , 'id_usuario': 1 , 'monto': 150.99, 'fecha':'2024-12-10'},
+{'id_transiccion' : 2 , 'id_usuario': 2 , 'monto': 89.99, 'fecha':'2024-12-09'},
+{'id_transiccion' : 3 , 'id_usuario': 1 , 'monto': 45.50, 'fecha':'2024-12-08'},
+
+
+
+
+
+]
+
+
+#Creando un endpoint para hacer pruebas..: endpoint: Raiz
 
 @aplicacion.get('/')
 
@@ -24,3 +47,14 @@ def leer_raiz():
     return {'Mensaje' : 'FlowConnect está funcionando correctamente'}
 
 
+#Endpoint Lista de usuarios:
+
+@aplicacion.get('/usuarios')
+def obtener_usuarios():
+    return usuarios
+
+#Endpoint para Lista de transacciones:
+
+@aplicacion.get('/transacciones')
+def obtener_transacciones():
+    return transacciones
