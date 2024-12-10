915626a4b4b60eb7080b0cf3024c57e28c0677a6
hayusom <hayusomartin@gmail.com>
2024-12-10 11:05:26 +0100

    configuracion inicial de API en main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #Nombre del proyecto: FlowConnect 
-
-
 #Autor: Héctor Ayuso Martín
 
 
+
 #FlowConnect fue elegido como nombre porque resalta la fluidez en la conexión entre backend y frontend.
+
+
+'''
+Confifuracion de main.
+Nucelo de la API.
+'''
+
+from fastapi import FastAPI
+
+aplicacion = FastAPI()
+
+
+#Creando un endpoint para hacer pruebas..:
+
+@aplicacion.get('/')
+
+def leer_raiz():
+    return {'Mensaje' : 'FlowConnect está funcionando correctamente'}
 
 
